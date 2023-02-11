a576e9b27967d23a73f15d6d9cd4cb7b75638756
Lucas Loureiro <lucas@DESKTOP-P0NEI7F>
2023-02-11 15:33:49 -0300

    feat: lb and asg done

---
+++ AWS/ecs-cluster-react/elb.tf
@@ -0,0 +1,53 @@
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group-lucas"
+  port        = "80"
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = aws_vpc.vpc.id
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    timeout             = 60
+    interval            = 300
+    matcher             = "200,301,302"
+  }
+}
+
+resource "aws_lb" "lb" {
+    name = "lucas-lb"
+    load_balancer_type = "application"
+    internal = false
+    subnets = [aws_subnet.subnet.id]
+    security_groups = [aws_security_group.lb_sg.id]
+  
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+
+
+resource "aws_security_group" "lb_sg" {
+  name   = "allow-all"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
